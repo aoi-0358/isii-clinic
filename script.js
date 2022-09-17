6b89b739f79818137a7f7526045f57818e3a4c0c
岡村　蒼 <okamuraao@okamuraaonoMacBook-Air.local>
2022-09-17 15:48:35 +0900

    可読性の向上

---
+++ script.js
@@ -0,0 +1,10 @@
+function loaded() {
+  document.getElementById("loading").classList.remove("active");
+}
+
+
+window.addEventListener("load", function() {
+  setTimeout(loaded, 500)
+})
+
+setTimeout(loaded, 5000)
