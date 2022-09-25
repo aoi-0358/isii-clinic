1f1501b71940e57cf836bdcf997a6198080f4ad6
aoi okamura <aogangcun@gmail.com>
2022-09-25 18:09:21 +0900

    保存

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function loaded() {
   document.getElementById("loading").classList.remove("active");
 }
-
-
 window.addEventListener("load", function() {
   setTimeout(loaded, 500)
 })
-
 setTimeout(loaded, 5000)
