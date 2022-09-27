c69439b6a1da9aadac086f5514f195f113d2b8af
aoi okamura <aogangcun@gmail.com>
2022-09-27 09:23:59 +0900

    animation

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ window.addEventListener("load", function() {
   setTimeout(loaded, 500)
 })
 setTimeout(loaded, 5000)
+
+
+
+
+const targetElement = document.querySelectorAll(".AnimationTarget");
+document.addEventListener("scroll", function() {
+  for (let i = 0; i < targetElement.length; i++) {
+    const getElementDistance = targetElement[i].
+    getBoundingClientRect().top + targetElement[i].clientHeight * .6
+    if (window.innerHeight > getElementDistance) {
+      targetElement[i].classList.add("show");
+    }
+  }
+})
